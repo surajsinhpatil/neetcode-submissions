ad3880072a87afcaa8a0db3c357293620e775112
Surajsinh Patil <surajpatilforwork@gmail.com>
2026-07-16 11:17:29 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxH;
+        for (int s : stones) {
+            maxH.push(s);
+        }
+
+        while (maxH.size() > 1) {
+            int first = maxH.top(); maxH.pop();
+            int second = maxH.top(); maxH.pop();
+            if (second < first) maxH.push(first - second);
+        }
+        maxH.push(0);
+        return maxH.top();
+    }
+};
